d3ede9f7fccf2fcb79f7311e36abc8bda6929a48
Marcin-Sk <suchszy@o2.pl>
2016-04-25 18:34:22 +0200

    Added files via upload

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+
+//comment
+namespace Aquapark
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            StreamReader myReader = new StreamReader("EntrysAndLeaves.txt");
+            string line = "";
+            line = myReader.ReadLine();
+            myReader.Close();
+
+            string eValue = line.Substring(2, 5);
+            string lValue = line.Substring(10, 5);
+            int clientValue = solution(eValue, lValue);
+            Console.WriteLine("You have to pay: " + clientValue + " Euro");
+            }
+
+            catch (DirectoryNotFoundException e) 
+            {
+                Console.WriteLine("Couldn't find the file. Check directory!");
+            }
+
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Couldn't find the file. Please check the filename.");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Something is wrong. Please check an error: {0}", e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int solution(string E, string L)
+        {
+            Program amount = new Program();
+            return amount.chargesCalc(E, L);
+        }
+
+        //Charges calculation:
+
+        public int chargesCalc(string E, string L)
+        {
+            int h = 0; //overall number of hours
+
+            int hEntry = int.Parse(E.Substring(0, 2));
+            int hLeave = int.Parse(L.Substring(0, 2));
+
+            int mEntry = int.Parse(E.Substring(3, 2));
+            int mLeave = int.Parse(L.Substring(3, 2));
+
+            h = hLeave - hEntry;
+
+            if (mEntry < mLeave)
+            {
+                h++;
+            }
+
+            if (h == 0)
+            {
+                return 0;
+            }
+
+            else if (h == 1)
+            {
+                return 4;
+            }
+
+            else
+            {
+                return (4 + 2 * (h - 1));
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1Tests/ProgramTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aquapark;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquapark.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        //time = 0h 0min
+
+        [TestMethod()]
+        public void chargesCalcTest()
+        {
+            Program target = new Program();
+            string E = "12:00";
+            string L = "12:00";
+            int expected = 0;
+            int actual;
+            actual = target.chargesCalc(E, L);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //time = 0h 10min
+
+        [TestMethod()]
+        public void chargesCalcTest2()
+        {
+            Program target = new Program();
+            string E = "12:00";
+            string L = "12:10";
+            int expected = 4;
+            int actual;
+            actual = target.chargesCalc(E, L);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //time = 2h 10min
+
+        [TestMethod()]
+        public void chargesCalcTest3()
+        {
+            Program target = new Program();
+            string E = "12:00";
+            string L = "14:10";
+            int expected = 8;
+            int actual;
+            actual = target.chargesCalc(E, L);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
